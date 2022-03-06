3d61ff415d4723d5e89d4d76aba37a7353075a55
Stuart Murray <stuart.murray.dev@outlook.com>
2022-03-06 15:32:22 +0000

    added useState

--- ReStore/client/src/App.tsx
+++ ReStore/client/src/App.tsx
@@ -1,7 +1,26 @@
+import { useState } from 'react';
+
 function App() {
+  const [products, setProducts] = useState([
+    { name: 'product1', price: 100.0 },
+    { name: 'product2', price: 200.0 },
+  ]);
+
+  function addProduct() {
+    setProducts([...products, { name: 'product3', price: 300.0 }]);
+  }
+
   return (
     <div>
       <h1>Re-Store</h1>
+      <ul>
+        {products.map((product, index) => (
+          <li key={index}>
+            {product.name} - {product.price}
+          </li>
+        ))}
+      </ul>
+      <button onClick={addProduct}>Add product</button>
     </div>
   );
 }

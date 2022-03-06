d0d38e9517af89f9f0f4924e78052efdfa21cc69
Stuart Murray <stuart.murray.dev@outlook.com>
2022-03-06 16:07:45 +0000

    use fetch to get products from Api. Also add CORS

--- ReStore/client/src/App.tsx
+++ ReStore/client/src/App.tsx
@@ -1,10 +1,16 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [products, setProducts] = useState([
     { name: 'product1', price: 100.0 },
     { name: 'product2', price: 200.0 },
   ]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/products')
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  }, []);
 
   function addProduct() {
     setProducts((prevstate) => [

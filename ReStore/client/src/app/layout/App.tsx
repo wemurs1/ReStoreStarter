054b6de53d2f7ead49cddca63cb3de70459c54c0
Stuart Murray <stuart.murray.dev@outlook.com>
2022-03-08 09:31:04 +0000

    working

--- ReStore/client/src/app/layout/App.tsx
+++ ReStore/client/src/app/layout/App.tsx
@@ -1,38 +1,22 @@
 import { Container, CssBaseline } from '@mui/material';
-import { useEffect, useState } from 'react';
+import { createTheme, ThemeProvider } from '@mui/system';
+import { useState } from 'react';
 import Catalog from '../../features/catalog/Catalog';
-import { Product } from '../models/product';
 import Header from './Header';
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([]);
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? 'dark' : 'light';
 
-  useEffect(() => {
-    fetch('http://localhost:5000/api/products')
-      .then((response) => response.json())
-      .then((data) => setProducts(data));
-  }, []);
-
-  function addProduct() {
-    setProducts((prevstate) => [
-      ...prevstate,
-      {
-        id: prevstate.length + 101,
-        name: 'product' + (prevstate.length + 1),
-        price: prevstate.length * 100 + 100,
-        brand: 'some brand',
-        description: 'some description',
-        pictureUrl: 'http://picsum.photos/200',
-      },
-    ]);
-  }
+  const theme = createTheme({
+  });
 
   return (
     <>
       <CssBaseline />
       <Header />
       <Container>
-        <Catalog products={products} addProduct={addProduct} />
+        <Catalog />
       </Container>
     </>
   );

de538ac58504bf5d5d62aecb6f546c79e3e39018
Stuart Murray <stuart.murray.dev@outlook.com>
2022-03-08 11:02:11 +0000

    checkpoint

--- ReStore/client/src/app/layout/App.tsx
+++ ReStore/client/src/app/layout/App.tsx
@@ -1,7 +1,12 @@
 import { Container, createTheme, CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@mui/system';
 import { useState } from 'react';
+import { Route } from 'react-router-dom';
+import AboutPage from '../../features/about/AboutPage';
 import Catalog from '../../features/catalog/Catalog';
+import ProductDetails from '../../features/catalog/ProductDetails';
+import ContactPage from '../../features/contact/ContactPage';
+import HomePage from '../../features/home/HomePage';
 import Header from './Header';
 
 function App() {
@@ -11,6 +16,9 @@ function App() {
   const theme = createTheme({
     palette: {
       mode: paletteType,
+      background: {
+        default: paletteType === 'light' ? '#eaeaea' : '#121212',
+      },
     },
   });
 
@@ -23,7 +31,11 @@ function App() {
       <CssBaseline />
       <Header darkmode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
-        <Catalog />
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/catalog' component={Catalog} />
+        <Route path='/catalog/:id' component={ProductDetails} />
+        <Route path='/about' component={AboutPage} />
+        <Route path='/contact' component={ContactPage} />
       </Container>
     </ThemeProvider>
   );
--- ReStore/client/src/app/layout/Header.tsx
+++ ReStore/client/src/app/layout/Header.tsx
@@ -1,16 +1,59 @@
-import { AppBar, Switch, Toolbar, Typography } from '@mui/material';
+import {
+  AppBar,
+  List,
+  ListItem,
+  Switch,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import { NavLink } from 'react-router-dom';
 
 interface Props {
   darkmode: boolean;
   handleThemeChange: () => void;
 }
 
-export default function Header({darkmode, handleThemeChange}: Props) {
+const midLinks = [
+  { title: 'catalog', path: '/catalog' },
+  { title: 'about', path: '/about' },
+  { title: 'contact', path: '/contact' },
+];
+
+const rightLinks = [
+  { title: 'login', path: '/login' },
+  { title: 'register', path: '/register' },
+];
+
+export default function Header({ darkmode, handleThemeChange }: Props) {
   return (
     <AppBar position='static' sx={{ mb: 4 }}>
       <Toolbar>
         <Typography variant='h6'>RE-STORE</Typography>
-        <Switch checked={darkmode} onChange={handleThemeChange}/>
+        <Switch checked={darkmode} onChange={handleThemeChange} />
+        <List sx={{ display: 'flex' }}>
+          {midLinks.map(({ title, path }) => (
+            <ListItem
+              component={NavLink}
+              to={path}
+              key={path}
+              sx={{ color: 'inherit', typography: 'h6' }}
+            >
+              {title.toUpperCase()}
+            </ListItem>
+          ))}
+        </List>
+        <List sx={{ display: 'flex' }}>
+          {rightLinks.map(({ title, path }) => (
+            <ListItem
+              component={NavLink}
+              to={path}
+              key={path}
+              sx={{ color: 'inherit', typography: 'h6' }}
+            >
+              {title.toUpperCase()}
+            </ListItem>
+          ))}
+        </List>
       </Toolbar>
     </AppBar>
   );

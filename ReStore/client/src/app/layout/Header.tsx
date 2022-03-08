5a60e1830366ed08893b995e81f88af111403fbb
Stuart Murray <stuart.murray.dev@outlook.com>
2022-03-08 13:53:26 +0000

    navlinks

--- ReStore/client/src/app/layout/Header.tsx
+++ ReStore/client/src/app/layout/Header.tsx
@@ -1,5 +1,8 @@
+import { ShoppingCart } from '@mui/icons-material';
 import {
   AppBar,
+  Badge,
+  IconButton,
   List,
   ListItem,
   Switch,
@@ -28,7 +31,14 @@ export default function Header({ darkmode, handleThemeChange }: Props) {
   return (
     <AppBar position='static' sx={{ mb: 4 }}>
       <Toolbar>
-        <Typography variant='h6'>RE-STORE</Typography>
+        <Typography
+          variant='h6'
+          component={NavLink}
+          to='/'
+          sx={{ color: 'inherit', textDecoration: 'none' }}
+        >
+          RE-STORE
+        </Typography>
         <Switch checked={darkmode} onChange={handleThemeChange} />
         <List sx={{ display: 'flex' }}>
           {midLinks.map(({ title, path }) => (
@@ -42,6 +52,11 @@ export default function Header({ darkmode, handleThemeChange }: Props) {
             </ListItem>
           ))}
         </List>
+        <IconButton size='large' sx={{ color: 'inherit' }}>
+          <Badge badgeContent={4} color='secondary'>
+            <ShoppingCart />
+          </Badge>
+        </IconButton>
         <List sx={{ display: 'flex' }}>
           {rightLinks.map(({ title, path }) => (
             <ListItem

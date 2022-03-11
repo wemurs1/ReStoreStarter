c1036d78c8b6a1b5b2cf1c1c6998290e2b02bab7
Stuart Murray <stuart.murray.dev@outlook.com>
2022-03-11 09:35:16 +0000

    not found component

--- ReStore/client/src/app/layout/App.tsx
+++ ReStore/client/src/app/layout/App.tsx
@@ -1,7 +1,7 @@
 import { Container, createTheme, CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@mui/system';
 import { useState } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import AboutPage from '../../features/about/AboutPage';
 import Catalog from '../../features/catalog/Catalog';
@@ -11,6 +11,7 @@ import HomePage from '../../features/home/HomePage';
 import Header from './Header';
 import 'react-toastify/dist/ReactToastify.css';
 import ServerError from '../errors/ServerError';
+import NotFound from '../errors/NotFound';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -35,12 +36,15 @@ function App() {
       <CssBaseline />
       <Header darkmode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/catalog' component={Catalog} />
-        <Route path='/catalog/:id' component={ProductDetails} />
-        <Route path='/about' component={AboutPage} />
-        <Route path='/contact' component={ContactPage} />
-        <Route path='/server-error' component={ServerError} />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/catalog' component={Catalog} />
+          <Route path='/catalog/:id' component={ProductDetails} />
+          <Route path='/about' component={AboutPage} />
+          <Route path='/contact' component={ContactPage} />
+          <Route path='/server-error' component={ServerError} />
+          <Route component={NotFound} />
+        </Switch>
       </Container>
     </ThemeProvider>
   );

7dbef05dc3e12420edd0d2ed617dd3bf7eaeae7c
Stuart Murray <stuart.murray.dev@outlook.com>
2022-03-11 09:21:01 +0000

    server error angular component

--- ReStore/client/src/app/api/agent.ts
+++ ReStore/client/src/app/api/agent.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosError, AxiosResponse } from 'axios';
 import { toast } from 'react-toastify';
+import { history } from '../..';
 
 axios.defaults.baseURL = 'http://localhost:5000/api/';
 
@@ -28,7 +29,7 @@ axios.interceptors.response.use(
         toast.error(data.title);
         break;
       case 500:
-        toast.error(data.title);
+        history.push({ pathname: '/server-error', state: { error: data } });
         break;
       default:
         break;
--- ReStore/client/src/app/errors/ServerError.tsx
+++ ReStore/client/src/app/errors/ServerError.tsx
@@ -1,11 +1,30 @@
-import { Container, Paper, Typography } from '@mui/material';
+import { Button, Container, Divider, Paper, Typography } from '@mui/material';
+import { useHistory, useLocation } from 'react-router-dom';
 
 export default function ServerError() {
+  const history = useHistory();
+  const { state } = useLocation<any>();
+
   return (
     <Container component={Paper}>
-      <Typography variant='h5' gutterBottom>
-        Server error
-      </Typography>
+      {state?.error ? (
+        <>
+          <Typography gutterBottom variant='h3' color='error'>
+            {state.error.title}
+          </Typography>
+          <Divider />
+          <Typography>
+            {state.error.detail || 'Internal Server Error'}
+          </Typography>
+        </>
+      ) : (
+        <Typography variant='h5' gutterBottom>
+          Server Error
+        </Typography>
+      )}
+      <Button onClick={() => history.push('/catalog')}>
+        Go back to the store
+      </Button>
     </Container>
   );
 }

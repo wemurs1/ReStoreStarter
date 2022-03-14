d92d54b271e5657c5b2146f50a3df56e4cd25b4b
Stuart Murray <stuart.murray.dev@outlook.com>
2022-03-14 16:08:53 +0000

    added back-end basket functionality

--- ReStore/API/Startup.cs
+++ ReStore/API/Startup.cs
@@ -52,7 +52,7 @@ namespace API
 
             app.UseCors(options =>
             {
-                options.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                options.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000");
             });
 
             app.UseAuthorization();

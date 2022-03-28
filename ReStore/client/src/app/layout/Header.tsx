aa9f0afb2da434fdb699ab8254ba41e25c2fa556
Stuart Murray <stuart.murray.dev@outlook.com>
2022-03-28 16:11:37 +0100

    handle login persistence

--- ReStore/client/src/app/layout/Header.tsx
+++ ReStore/client/src/app/layout/Header.tsx
@@ -12,6 +12,7 @@ import {
 } from '@mui/material';
 import { Link, NavLink } from 'react-router-dom';
 import { useAppSelector } from '../store/configureStore';
+import SignedInMenu from './SignedInMenu';
 
 interface Props {
   darkmode: boolean;
@@ -39,6 +40,7 @@ const navStyles = {
 
 export default function Header({ darkmode, handleThemeChange }: Props) {
   const { basket } = useAppSelector((state) => state.basket);
+  const { user } = useAppSelector((state) => state.account);
   const itemCount = basket?.items.reduce((sum, item) => sum + item.quantity, 0);
 
   return (
@@ -77,13 +79,22 @@ export default function Header({ darkmode, handleThemeChange }: Props) {
               <ShoppingCart />
             </Badge>
           </IconButton>
-          <List sx={{ display: 'flex' }}>
-            {rightLinks.map(({ title, path }) => (
-              <ListItem component={NavLink} to={path} key={path} sx={navStyles}>
-                {title.toUpperCase()}
-              </ListItem>
-            ))}
-          </List>
+          {user ? (
+            <SignedInMenu />
+          ) : (
+            <List sx={{ display: 'flex' }}>
+              {rightLinks.map(({ title, path }) => (
+                <ListItem
+                  component={NavLink}
+                  to={path}
+                  key={path}
+                  sx={navStyles}
+                >
+                  {title.toUpperCase()}
+                </ListItem>
+              ))}
+            </List>
+          )}
         </Box>
       </Toolbar>
     </AppBar>

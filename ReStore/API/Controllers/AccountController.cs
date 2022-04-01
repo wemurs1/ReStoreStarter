40db0ecd7ad80f9ebc3087ec97263205fccb37da
Stuart Murray <stuart.murray.dev@outlook.com>
2022-04-01 16:30:36 +0100

    checkpoint

--- ReStore/API/Controllers/AccountController.cs
+++ ReStore/API/Controllers/AccountController.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
@@ -94,6 +95,16 @@ namespace API.Controllers
                 Basket = userBasket?.MapBasketToDto()
             };
         }
+
+        [Authorize]
+        [HttpGet("savedAddress")]
+        public async Task<ActionResult<UserAddress>> GetSavedAddress()
+        {
+            return await _userManager.Users
+                .Where(x => x.UserName == User.Identity.Name)
+                .Select(user => user.Address)
+                .FirstOrDefaultAsync();
+        } 
 
         private async Task<Basket> RetrieveBasket(string buyerId)
         {
--- ReStore/client/src/app/components/AppCheckbox.tsx
+++ ReStore/client/src/app/components/AppCheckbox.tsx
@@ -3,13 +3,21 @@ import { useController, UseControllerProps } from 'react-hook-form';
 
 interface Props extends UseControllerProps {
   label: string;
+  disabled: boolean;
 }
 
 export default function AppCheckbox(props: Props) {
   const { field } = useController({ ...props, defaultValue: false });
   return (
     <FormControlLabel
-      control={<Checkbox {...field} checked={field.value} color='secondary' />}
+      control={
+        <Checkbox
+          {...field}
+          checked={field.value}
+          color='secondary'
+          disabled={props.disabled}
+        />
+      }
       label={props.label}
     />
   );

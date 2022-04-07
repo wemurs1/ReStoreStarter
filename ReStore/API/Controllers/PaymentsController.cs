3fc483e306d53ce180d5b27584e4108a87b9c874
Stuart Murray <stuart.murray.dev@outlook.com>
2022-04-07 17:14:42 +0100

    stripe webhook

--- ReStore/API/Controllers/PaymentsController.cs
+++ ReStore/API/Controllers/PaymentsController.cs
@@ -1,14 +1,18 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
+using API.Entities.OrderAggregate;
 using API.Extensions;
 using API.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Stripe;
 
 namespace API.Controllers
 {
@@ -16,11 +20,13 @@ namespace API.Controllers
     {
         private readonly PaymentService _paymentService;
         private readonly StoreContext _context;
+        private readonly IConfiguration _config;
 
-        public PaymentsController(PaymentService paymentService, StoreContext context)
+        public PaymentsController(PaymentService paymentService, StoreContext context, IConfiguration config)
         {
             _paymentService = paymentService;
             _context = context;
+            _config = config;
         }
 
         [Authorize]
@@ -45,6 +51,31 @@ namespace API.Controllers
             if (!result) return BadRequest(new ProblemDetails { Title = "Problem updating basket with intent" });
 
             return basket.MapBasketToDto();
+        }
+
+        [HttpPost("webhook")]
+        public async Task<ActionResult> StripeWebhook()
+        {
+            var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+
+            var stripeEvent = EventUtility.ConstructEvent(
+                json,
+                Request.Headers["Stripe-Signature"],
+                _config["StripeSettings:WhSecret"]
+            );
+
+            var charge = (Charge)stripeEvent.Data.Object;
+
+            var order = await _context.Orders.FirstOrDefaultAsync(x => x.PaymentIntentId == charge.PaymentIntentId);
+
+            if (charge.Status == "succeeded")
+            {
+                order.OrderStatus = OrderStatus.PaymentReceived;
+            }
+
+            await _context.SaveChangesAsync();
+
+            return new EmptyResult();
         }
     }
 }
